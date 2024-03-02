7ca750a2602d1b8ea8e656ed3076783da8759b68
Abderrazak LAANAYA <laanayabdrzak@gmail.com>
2024-03-02 21:59:46 +0100

    Initial Commit

---
+++ TooltipsSample/TooltipsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TooltipsSampleApp.swift
+//  TooltipsSample
+//
+//  Created by LAANAYA Abderrazak on 2/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct TooltipsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
